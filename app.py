58ab8ae3a27df4013f0522ca10376504f9701648
Wakaru Himura <wakaru44@gmail.com>
2016-01-25 00:36:56 +0100

    pep8

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ if __name__ == '__main__':
     if not app.debug:
         import logging
         from logging.handlers import TimedRotatingFileHandler
-        file_handler = TimedRotatingFileHandler("abapi.log",when="D", backupCount=10)
+        file_handler = TimedRotatingFileHandler(
+            "dockerplace.log",
+            when="D",
+            backupCount=10)
         file_handler.setLevel(logging.WARNING)
         app.logger.addHandler(file_handler)
 
     app.debug = app.config['DEBUG']
     app.run()
-
-
--- dockerplace/__init__.py
+++ dockerplace/__init__.py
@@ -5,4 +5,3 @@ app = Flask(__name__)
 app.config.from_object('settings')
 csrf(app)
 from dockerplace import main
-
