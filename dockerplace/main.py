d8c543bde22c3e9edeb7a499fa14845fb7f79b60
Juan Morales <juan.morales@picsolve.com>
2016-01-25 18:28:01 +0000

    removing useless helpers

--- dockerplace/main.py
+++ dockerplace/main.py
@@ -4,6 +4,7 @@ from flask import Flask, url_for, render_template, request, \
     redirect, abort, session, g, flash, Markup
 from dockerplace import app
 from dockerplace.system_calls import get_all_actions, run_make_action
+from dockerplace.system_calls import do_run
 
 
 @app.route('/')
@@ -16,13 +17,19 @@ def desktop():
     """
     this route displays a list of available services to the user
     """
+    dhome = app.config["DOCKER_SERVICES_HOME"] 
     return render_template(
         "desktop.html",
         data={
-            "services": get_all_actions(app.config["DOCKER_SERVICES_HOME"]),
-            "debug": app.config["DEBUG"]
+            "services": get_all_actions(dhome),
+            "debug": {
+                "enabled": app.config["DEBUG"],
+                "data": {
+                    "docker_home": dhome,
+                    "listing": do_run("cat {0}/docker.compose_ui/Makefile".format(dhome))}
             }
-        )
+        }
+    )
 
 
 @app.route('/quickstart')
