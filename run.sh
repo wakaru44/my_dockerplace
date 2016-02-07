51d4f318b57e65719ac8d5e2b62ed87f6a6fcd74
Wakaru Himura <wakaru44@gmail.com>
2016-02-07 20:45:04 +0100

    fixed issue with missing icons when running from desktop launcher

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 APP_HOME="/home/wakaru/workspace/src/python/my_dockerplace"
+cd $APP_HOME
+echo "Starting app in $(pwd)" >> $APP_HOME/app.$(date +%F).log 
 nohup /usr/bin/python $APP_HOME/app.py 2>&1 | tee -a $APP_HOME/app.$(date +%F).log &
 xdg-open http://localhost:8080    
